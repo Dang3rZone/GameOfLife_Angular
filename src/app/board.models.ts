93b979c3040a723fcefb07975510759ed2101a60
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-12 21:19:34 +0100

    status, changeStatus, checkBoard, Checkrules

--- src/app/board.models.ts
+++ src/app/board.models.ts
@@ -3,5 +3,36 @@ export class Board {
 
   constructor(width, height) {
     this.board = [];
+    for (let i = 0; i < width; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < height; j++) {
+        this.board[i][j] = 0;
+      }
+    }
+  }
+
+  status(coordX: number, coordY: number): number {
+    return this.board[coordX][coordY];
+  }
+
+  changeStatus(coordX: number, coordY: number) {
+    this.board[coordX][coordY] = this.board[coordX][coordY] === 0 ? 1 : 0;
+  }
+
+  checkBoard() {
+    let tempBoard = [];
+    for (let i = 0; i < this.board.length; i++) {
+      tempBoard[i] = [];
+      for (let j = 0; j < this.board[i].length; j++) {}
+    }
+  }
+
+  checkRules(coordX: number, coordY: number): number {
+    const width = this.board.length;
+    const height = this.board[0].length;
+
+    const xMenos = coordX - 1 < 0 ? width - 1 : coordX - 1;
+    const xMaas = coordX + 1 >= width ? 0 : coordX - 1;
+    return;
   }
 }
